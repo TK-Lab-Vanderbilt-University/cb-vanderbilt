7ce278f45a4bd9f9b4ca98a1e3a1165bc16ca77c
TK-Lab-Vanderbilt-University <99231517+TK-Lab-Vanderbilt-University@users.noreply.github.com>
2026-02-27 12:10:10 -0600

    Add files via upload

---
+++ R/CompactSeurat.R
@@ -0,0 +1,34 @@
+#' Compact Seurat Object by Removing Unnecessary Data and Coercing to Sparse Matrix
+#'
+#' @param seurat_obj A Seurat object.
+#'
+#' @return A compacted Seurat object with normalized RNA assay.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' obj <- CompactSeurat(seurat_obj)
+#' }
+CompactSeurat = function(seurat_obj) {
+  DefaultAssay(seurat_obj) <- "RNA"
+  if (inherits(seurat_obj[["RNA"]], "Assay5")) {
+    seurat_obj = JoinLayers(seurat_obj)
+  }
+
+  seurat_obj = DietSeurat(
+    object = seurat_obj,
+    assays = "RNA",
+    layers = c("counts", "data"),
+    dimreducs = c("pca", "harmony", "umap")
+  )
+
+  if (is(seurat_obj[["RNA"]]$counts, "matrix")) {
+    seurat_obj[["RNA"]]$counts <- as(seurat_obj[["RNA"]]$counts, "dgCMatrix")
+  }
+  if (is(seurat_obj[["RNA"]]$data, "matrix")) {
+    seurat_obj[["RNA"]]$data <- as(seurat_obj[["RNA"]]$data, "dgCMatrix")
+  }
+
+  seurat_obj = NormalizeData(seurat_obj)
+  return(seurat_obj)
+}
